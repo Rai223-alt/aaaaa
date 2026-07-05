b2da1531b11e4453b7a58f0efcf74e346024702f
Raime <rai2xxx.1213@gmail.com>
2026-07-06 01:13:18 +0900

    私たちの修正

---
+++ index4.js
@@ -0,0 +1,29 @@
+[33mcommit c2473396d3d0d65b424242ba6ac512a3254e934c[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m)[m
+Author: Raime <rai2xxx.1213@gmail.com>
+Date:   Mon Jul 6 01:07:29 2026 +0900
+
+    logを削除
+
+[33mcommit 89d754106b3ccac013f5ff800e0aa68e50b10053[m
+Author: Raime <rai2xxx.1213@gmail.com>
+Date:   Mon Jul 6 01:06:08 2026 +0900
+
+    logを追加
+
+[33mcommit 70352a5c8bfc2e26c1fb8efa38ae3b7f4e1942bd[m
+Author: Raime <rai2xxx.1213@gmail.com>
+Date:   Mon Jul 6 01:04:27 2026 +0900
+
+    秘密のファイルの削除
+
+[33mcommit fc5b20e673b5cc349248045f899ab6e32fd14d4d[m
+Author: Raime <rai2xxx.1213@gmail.com>
+Date:   Mon Jul 6 00:59:14 2026 +0900
+
+    いろいろ開発した
+
+[33mcommit 218b8cceaf08ebf168d3b5f63670d788944b5602[m
+Author: Raime <rai2xxx.1213@gmail.com>
+Date:   Mon Jul 6 00:30:35 2026 +0900
+
+    初めてのコミット
